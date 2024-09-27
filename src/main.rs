41ff33bf94ba9f8a8a3123ce3f5f4b91ddc2ae21
chemicstry <chemicstry@gmail.com>
2024-09-27 18:59:56 +0300

    Try to reimplement net

--- src/main.rs
+++ src/main.rs
@@ -15,15 +15,63 @@ use esp_backtrace as _;
 use esp_hal::{
     clock::ClockControl,
     peripherals::Peripherals,
+    rng::Rng,
     system::SystemControl,
-    timer::timg::TimerGroup,
+    timer::{systimer::SystemTimer, timg::TimerGroup},
 };
+use esp_println::println;
+use esp_wifi::{
+    wifi::{
+        ClientConfiguration, Configuration, WifiController, WifiEvent, WifiStaDevice, WifiState,
+    },
+    EspWifiInitFor,
+};
+
+const SSID: &str = "TuRgElIs";
+const PASSWORD: &str = "TREX600NPRO";
 
 #[embassy_executor::task]
 async fn run() {
     loop {
         esp_println::println!("Hello world from embassy using esp-hal-async!");
         Timer::after(Duration::from_millis(1_000)).await;
+    }
+}
+
+// maintains wifi connection, when it disconnects it tries to reconnect
+#[embassy_executor::task]
+async fn connection(mut controller: WifiController<'static>) {
+    println!("start connection task");
+    println!("Device capabilities: {:?}", controller.get_capabilities());
+    loop {
+        match esp_wifi::wifi::get_wifi_state() {
+            WifiState::StaConnected => {
+                // wait until we're no longer connected
+                controller.wait_for_event(WifiEvent::StaDisconnected).await;
+                Timer::after(Duration::from_millis(5000)).await
+            }
+            _ => {}
+        }
+        if !matches!(controller.is_started(), Ok(true)) {
+            let client_config = Configuration::Client(ClientConfiguration {
+                ssid: SSID.try_into().unwrap(),
+                password: PASSWORD.try_into().unwrap(),
+                ..Default::default()
+            });
+            controller.set_configuration(&client_config).unwrap();
+            println!("Starting wifi");
+            controller.start().await.unwrap();
+            println!("Wifi started!");
+        }
+        println!("About to connect...");
+
+        match controller.connect().await {
+            Ok(_) => println!("Wifi connected!"),
+            Err(e) => {
+                println!("Failed to connect to wifi: {e:?}");
+                Timer::after(Duration::from_millis(5000)).await
+            }
+        }
     }
 }
 
@@ -38,6 +86,29 @@ async fn main(spawner: Spawner) {
 
     let timg0 = TimerGroup::new(peripherals.TIMG0, &clocks);
     esp_hal_embassy::init(&clocks, timg0.timer0);
+
+    let wifi_init = esp_wifi::initialize(
+        EspWifiInitFor::Wifi,
+        timg0.timer0,
+        Rng::new(peripherals.RNG),
+        peripherals.RADIO_CLK,
+        &clocks,
+    )
+    .expect("Failed to initialize Wifi");
+
+    let wifi = peripherals.WIFI;
+    let (wifi_interface, controller) =
+        esp_wifi::wifi::new_with_mode(&wifi_init, wifi, WifiStaDevice).unwrap();
+
+    let config = embassy_net::Config::dhcpv4(Default::default());
+
+    let seed = 1234; // very random, very secure seed
+
+    let mut resources = embassy_net::StackResources::<3>::new();
+    // Init network stack
+    let stack = embassy_net::Stack::new(wifi_interface, config, &mut resources, seed);
+
+    spawner.spawn(connection(controller)).ok();
 
     spawner.spawn(run()).ok();
 
